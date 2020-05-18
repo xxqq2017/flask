5eb9be22604f79d7813f4171bcaeb2f59789aa63
xqing <xxqq2017@qq.com>
2020-05-18 18:33:30 +0800

    db one.

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy #数据库类导入
+import os, sys
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='nihao'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # 关闭对模型修改的监控
+db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
 
 
 @app.route('/')
