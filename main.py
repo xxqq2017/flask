da9672aeeb5507f9148db2c5298e6e5c5f8b2778
xqing <xxqq2017@qq.com>
2020-05-18 17:02:05 +0800

    500 error add!

--- main.py
+++ main.py
@@ -12,5 +12,10 @@ def index():
 def page_not_found(error):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(host='192.168.50.222',port=80, debug=True)
