f1582cf0b6e8f1a5c5f03af9b65bab397567faaf
xqing <xxqq2017@qq.com>
2020-05-18 17:22:30 +0800

    new index

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY']='nihao'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',name=name,movies=movies)
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -16,6 +16,19 @@ def page_not_found(error):
 def server_error(error):
     return render_template('500.html'), 500
 
+name = 'xiangqing'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 if __name__ == '__main__':
     app.run(host='192.168.50.222',port=80, debug=True)
