408afb9f5ce3a96537b3c63afc930b22cb61ebc9
xqing <xxqq2017@qq.com>
2020-05-25 15:40:13 +0800

    all

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+import click
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect,url_for
+import config
+import os, sys, pymysql
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy #数据库类导入
-import os, sys
-import pymysql
-
 
 WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
@@ -12,21 +12,89 @@ else:  # 否则使用四个斜线
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='nihao'
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+# app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
+#app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # 关闭对模型修改的监控
-db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app
+# db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app
 
-@app.route('/')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     conn = pymysql.connect(host='43.255.231.253', user='root',
-                           password='2468QAZwsx@', db='liutuwang',
+                           password='2468QAZwsx@', db='test',
                            charset='utf8')
     cur = conn.cursor()
-    sql = "SELECT username,email,user_pass FROM cdb_user where email != '' limit 100"
+    sql = "SELECT * FROM movie"
     cur.execute(sql)
-    u = cur.fetchall()
-    conn.close()
-    return render_template('index.html',movies=u)
+    movies = cur.fetchall()
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        id = request.form.get('id')
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or not id or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        sql_update="insert into movie (id,title,year) values(%s,%s,%s)"%(id,title,year)
+        cur.execute(sql_update)
+        conn.commit()  # 提交数据库会话
+        conn.close()
+        flash('Item created.')  # 显示成功创建的提示
+    return render_template('index.html', movies=movies)
+
+
+@app.route('/movie/edit/<movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    conn = pymysql.connect(host='43.255.231.253', user='root',
+                           password='2468QAZwsx@', db='test',
+                           charset='utf8')
+    cur = conn.cursor()
+    sql = "SELECT * FROM movie where id =%s"%movie_id
+    cur.execute(sql)
+    movies = cur.fetchone()
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+        sql2 = "update movie set title=%s ,year = %s where id=%s" %(title,year,movie_id)
+        cur.execute(sql2)
+        conn.commit()
+        conn.close() # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+    #return render_template(url_for('edit', movie_id=movie_id,movie=movies)) # 传入被编辑的电影记录
+    return render_template('edit.html', movie=movies) # 传入被编辑的电影记录
+
+
+@app.route('/movie/delete/<movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    conn = pymysql.connect(host='43.255.231.253', user='root',
+                           password='2468QAZwsx@', db='test',
+                           charset='utf8')
+    cur = conn.cursor()
+    sql_delete = "delete FROM movie where id=%s"%movie_id
+    cur.execute(sql_delete)
+    conn.commit()
+    flash('Item deleted.')
+    return render_template(url_for('index'))  # 重定向回主页
+
+@app.route('/upload',methods=['POST','GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        UPLOAD_FOLDER = 'c:\data\project\env'
+        ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+        MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
+        f.save(UPLOAD_FOLDER/f.filename)
+        flash('file uploaded successfully')
+        return render_template('upload.html')
+    return render_template('upload.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -39,14 +107,14 @@ def server_error(error):
 @app.context_processor
 def t_context():
     conn = pymysql.connect(host='43.255.231.253', user='root',
-                           password='2468QAZwsx@', db='liutuwang',
+                           password='2468QAZwsx@', db='test',
                            charset='utf8')
     cur = conn.cursor()
-    sql = "SELECT username FROM cdb_user where email != '' limit 1"
+    sql = "SELECT * FROM user"
     cur.execute(sql)
     names = cur.fetchone()
     conn.close()
-    return dict(name=names[0])
+    return dict(name=names[1])
 
 #name = 'xiangqing'
 names={'name':'xiangqing'}
